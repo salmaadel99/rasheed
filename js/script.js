6260ed69accb88c10a162d4a8494b7f8e07c7246
Salma <salmaadelmans@std.edu.eg>
2021-07-15 18:39:59 +0200

    rashed project

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$(' .owl-carousel').owlCarousel({
+    loop: true,
+    dots: false,
+    responsiveClass: true,
+    navText: ['<span class="fas fa-chevron-left fa-2x"></span>', '<span class="fas fa-chevron-right fa-2x"></span>'],
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 1,
+            nav: true
+        },
+        1000: {
+            items: 1,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
+$('.multiple-items').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
